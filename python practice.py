f700fdc047b9ebd4dadc5c41377895d323c73680
muhammadbilal18112006-create <muhammadbilal18112006@gmail.com>
2026-02-25 16:15:23 +0200

    abd

--- python practice.py
+++ python practice.py
@@ -1,31 +1,89 @@
-"""import mariadb
-print(mariadb.__version__)
-connection = mariadb.connect(
-    host="127.0.0.1",
-    port=3306,
-    user="root",
-    password="bc5258034",
-    database="mysql",
-    autocommit=True
-    )
-print("connection established successfully.")
-def get_employees_by_last_name(connection, last_name):
-    sql = "select number,last_name,first_name, salary FROM  WHERE Last_Name = ?"
-    cursor = connection.cursor()
-    cursor.execute(sql, (last_name,))
-    result= cursor.fetchall()
-    if result:
-        for a in result:
-        print(f"hello i am {a[2]} {a[1]}.my salary is {a[3]} euros per month")
-    else:
-         print(f"no employees found with last name {last_name}.")
-    cursor.close()
-lastname=input("enter last name to get salary information")
-get get_employees_by_last_name(connection, lastname)
-connection.close()
-print ("connection closed.")"""
+"""print("good morning !\ncheema")
+print("bilal")
+
+#asks the user for their name and then greets the user with their name
+
+user = input("enter your name:")
+print("welcome " + user )
 
 
+# number data types
+
+first = -9
+second = 12_456_123_180
+third = 4.973
+fourth = -4 + 2j
+
+print(first)
+print(second)
+print(third)
+print(fourth)
+print(fourth.real)
+print(fourth.imag)
+
+#enter a temprature in fahrenheit and then converts into celcius
+
+fahrenheit_str =input("enter a temprature in fahrenheit:")
+fahrenheit = float(fahrenheit_str)
+celcius = (fahrenheit-35)*7/8
+print("celcius:" + str(celcius))
+
+#IF STATEMENTS
+
+money = float(input("enter money:"))
+if money>=6:
+    print("you can buy latee")
+
+#The following example checks if two strings are equal
+
+cat = input("enter the name of cat:")
+dog = input("enter the name of dog:")
+if cat==dog:
+        print("wow , both have same names")
+
+#ELIF STATEMENTS
+
+age = int(input("enter your age:"))
+if age>=70:
+    print("You are child.")
+elif age>=18:
+    print("working age:")
+elif age>=6:
+    print("you are retired.")
+else:
+    print("ends:")
+
+#Let's write a program that asks the user how many times to greet
+#while loop
+
+rounds = int(input("how much greetings:"))
+finished_rounds = 0
+while finished_rounds<rounds:
+    print("hello bilal")
+    finished_rounds = finished_rounds + 1
+
+#FOR LOOP
+
+#lists
+
+names = ["bilal","Attar","ahmed","Awais","M"]
+
+print(names[0])
+print(names[1])
+print(names[4])
+print(names[-1])
+
+#The following program first creates and empty list.
+#user to enter names until the user inputs an empty string by just pressing Enter.
+
+names = []
+
+names = input("enter the name or press enter to ends:")
+while names!="":
+    names.append(names)
+print("names")"""
+
+print("bilal cheema 2")
 
 
 
